eb4c161ff94e84474f81377cf314533fb78a938f
Aris Dela Cruz <ardelacruz@mssngsolutions.ph>
2017-02-12 00:15:55 +0100

    Added Spring Web and Thymeleaf.

---
+++ src/main/java/com/ariscdc/udemy/wsas/UdemyWebappSpringAwsStripeApplication.java
@@ -0,0 +1,12 @@
+package com.ariscdc.udemy.wsas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyWebappSpringAwsStripeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyWebappSpringAwsStripeApplication.class, args);
+	}
+}
